c12f47b89fc16311a3e8168b6e133eb3e3ea4973
hzxfelps <felipe.augusto.gonzales1a@gmail.com>
2026-04-20 23:43:55 -0300

    Delete Makerthon directory

--- Makerthon/esp32/wifi_config.h
+++
@@ -1,8 +0,0 @@
-#ifndef WIFI_CONFIG_H
-#define WIFI_CONFIG_H
-
-const char* ssid = "SEU_WIFI";
-const char* password = "SUA_SENHA";
-const char* serverName = "http://192.168.0.100:5000/data";
-
-#endif
--- Makerthon/localserver/priority_queue.py
+++
@@ -1,6 +0,0 @@
-def get_next_group(grupos):
-    if not grupos:
-        return None
-
-    # menor nível = maior prioridade
-    return min(grupos, key=grupos.get)
