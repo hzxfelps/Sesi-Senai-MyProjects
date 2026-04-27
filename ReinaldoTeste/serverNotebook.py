0bbb635c699dc55fde29e4f3e62d44c2f9993882
hzxfelps <felipe.augusto.gonzales1a@gmail.com>
2026-04-27 15:48:31 -0300

    Update serverNotebook.py

--- ReinaldoTeste/serverNotebook.py
+++ ReinaldoTeste/serverNotebook.py
@@ -16,15 +16,15 @@ def laser():
 def biologia():
     return render_template("biologia.html")
 
-@app.route("/estado")
-def estado():
+@app.route("/estado", methods=["GET"])
+def get_estado():
+    return jsonify({"ouvindo": estado_escuta})
+
+@app.route("/estado", methods=["POST"])
+def set_estado():
     global estado_escuta
-
-    ouvir = request.args.get("ouvindo")
-
-    if ouvir is not None:
-        estado_escuta = (ouvir == "1")
-
+    data = request.get_json()
+    estado_escuta = data.get("ouvindo", False)
     return jsonify({"ouvindo": estado_escuta})
 
 app.run(host="0.0.0.0", port=5000)
